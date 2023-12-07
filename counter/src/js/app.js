ddde638f12b581acb46a21acbf0ad450b061c546
Mariana Batista <mariana-batista99@hotmail.com>
2023-12-07 10:06:01 -0300

    :sparkles: project 2

--- counter/src/js/app.js
+++ counter/src/js/app.js
@@ -1,37 +1,68 @@
 /*** ===== minha forma de fazer ===== ****/
-let count = 0;
+// let count = 0;
 
-const value = document.getElementById('value');
-const decrease = document.querySelector('.decrease');
-const increase = document.querySelector('.increase');
-const reset = document.querySelector('.reset');
+// const value = document.getElementById('value');
+// const decrease = document.querySelector('.decrease');
+// const increase = document.querySelector('.increase');
+// const reset = document.querySelector('.reset');
 
-decrease.addEventListener('click', function () {
-  count -= 1;
-  value.textContent = count;
-  if (count < 0 && count !== 0) {
-    value.style.color = 'red';
-  } else if (count === 0) {
-    value.style.color = 'black';
-  }
-});
+// decrease.addEventListener('click', function () {
+//   count -= 1;
+//   value.textContent = count;
+//   if (count < 0 && count !== 0) {
+//     value.style.color = 'red';
+//   } else if (count === 0) {
+//     value.style.color = 'black';
+//   }
+// });
 
-increase.addEventListener('click', function () {
-  count += 1;
-  value.textContent = count;
-  if (count > 0) {
-    value.style.color = 'green';
-  } else if (count === 0) {
-    value.style.color = 'black';
-  }
-});
+// increase.addEventListener('click', function () {
+//   count += 1;
+//   value.textContent = count;
+//   if (count > 0) {
+//     value.style.color = 'green';
+//   } else if (count === 0) {
+//     value.style.color = 'black';
+//   }
+// });
 
-reset.addEventListener('click', function () {
-  count = 0;
-  value.textContent = count;
-  if (count === 0) {
-    value.style.color = 'black';
-  }
-});
+// reset.addEventListener('click', function () {
+//   count = 0;
+//   value.textContent = count;
+//   if (count === 0) {
+//     value.style.color = 'black';
+//   }
+// });
 
 /*** ===== forma video ===== ****/
+//set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector("#value")
+const btns = document.querySelectorAll('.btn')
+
+btns.forEach(function(btn){
+  btn.addEventListener('click', function(e) {
+    const styles = e.currentTarget.classList
+    if(styles.contains('decrease')){
+      count--
+    } else if(styles.contains('increase')){
+      count++
+    } else {
+      count = 0
+    }
+
+    if(count > 0){
+      value.style.color = 'green'
+    }
+    if(count < 0){
+      value.style.color = 'red'
+    }
+    if(count === 0){
+      value.style.color = '#222';
+    }
+    value.textContent = count
+    
+  })
+})
